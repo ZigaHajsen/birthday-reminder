2754c7b96fd880a75361fc0b46fa48c758f1e963
Žiga Hajšen <ziga.hajsen@gmail.com>
2020-10-30 19:49:42 +0100

    Redux setup

--- src/index.tsx
+++ src/index.tsx
@@ -1,13 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-
+import { Provider } from 'react-redux';
+import store from './redux';
 import { GlobalStyles } from './GlobalStyles';
 import { App } from './components';
 
 ReactDOM.render(
   <React.StrictMode>
     <GlobalStyles />
-    <App />
+    <Provider store={store}>
+      <App />
+    </Provider>
   </React.StrictMode>,
   document.getElementById('root')
 );
---
+++ src/redux/reducers/appReducer.ts
@@ -0,0 +1,5 @@
+import data from '../../data.json';
+
+export default function appReducer(state = data, action: string) {
+  return state;
+}
